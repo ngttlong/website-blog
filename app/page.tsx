5e67c68b00c75e653159fa3fc1261b43c4fb8d4c
Nguyễn Tài Long <ngtlong110905@gmail.com>
2026-04-25 00:19:13 +0700

    Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-// app/page.tsx
 import { HeroSection } from "@/components/sections/HeroSection/HeroSection";
-// import { CategorySection } from "@/components/sections/CategorySection/CategorySection";
 import { LatestChronicles } from "@/components/sections/LatestChronicles/LatestChronicles";
 import { Newsletter } from "@/components/sections/Newsletter/Newsletter";
 import { Footer } from "@/components/layout/Footer/Footer"; 
@@ -9,7 +7,6 @@ export default function Home() {
   return (
     <>
       <HeroSection />
-      {/* <CategorySection /> */}
       <LatestChronicles />
       <Newsletter />
       <Footer />
